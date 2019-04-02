fa8c5acd8fb9bff8ac7eaf2e7deaa12127f04eda
RoguePY <40289304+RoguePY@users.noreply.github.com>
2019-04-01 23:18:55 -0500

    Update client.py

--- client.py
+++ client.py
@@ -121,7 +121,7 @@ async def giveaway(ctx, secondtime, *, content:str):
 		
 @client.command()
 async def owo(ctx):
-	await client.send_message(msg.channel, "** The winner is @Yunt#9867. Congratulations! Hold your horses though, cause they might not have 5 invites!**")
+	await client.send_message(client.get_channel('561435469699612673'), "** The winner is @Yunt#9867. Congratulations! Hold your horses though, cause they might not have 5 invites!**")
 		
 @client.command(pass_context=True)
 async def listeningpresence(ctx, *, content:str):
