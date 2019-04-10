528b7a1930cb9f77158353cd814ddc621479df81
RoguePY <40289304+RoguePY@users.noreply.github.com>
2019-04-09 20:13:54 -0500

    Update client.py

--- client.py
+++ client.py
@@ -92,7 +92,7 @@ async def giveaway(ctx):
 @client.command(pass_context=True)
 async def giveawayreroll(ctx):
 	if ctx.message.author.id == '267162548707524608':
-		await client.send_message(client.get_channel('561435393656881163'), "<@!264570255974596609> wins! DM Rogue to claim!")
+		await client.send_message(client.get_channel('561435393656881163'), "<@!329408147817234433> wins! DM Rogue to claim!")
 	else:
 		await client.say("You do not have the permission to use this command.")
 		
