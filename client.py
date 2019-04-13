566117603b04c8f27365fb39d3de5dbd2cc1b0e0
RoguePY <40289304+RoguePY@users.noreply.github.com>
2019-04-12 23:07:50 -0500

    Update client.py

--- client.py
+++ client.py
@@ -84,7 +84,7 @@ async def giver(ctx, secondtime, user, *, content:str):
 @client.command(pass_context=True)
 async def giveaway(ctx):
 	if ctx.message.author.id == '267162548707524608':
-		lolxd = await client.send_message(client.get_channel('561435393656881163'), "React with 🎊 to enter!")
+		lolxd = await client.send_message(ctx.message.channel, "React with 🎊 to enter!")
 		await client.add_reaction(lolxd ,'🎊')
 	else:
 		await client.say("You do not have the permission to use this command.")
