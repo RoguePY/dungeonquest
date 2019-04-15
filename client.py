021f9bdc46bf4cf6ada20a5965d88d8d2e91a585
RoguePY <40289304+RoguePY@users.noreply.github.com>
2019-04-15 03:21:59 -0500

    Update client.py

--- client.py
+++ client.py
@@ -117,4 +117,10 @@ async def join(ctx):
 	channel = ctx.message.author.voice.voice_channel
 	await client.join_voice_channel(channel)
 	
+@client.command(pass_context=True)
+async def leave(ctx):
+	server = ctx.message.server
+	voice_client = client.voice_client_in(server)
+	await voice_client.disconnect()
+
 client.run(os.environ['BOT_TOKEN'])
